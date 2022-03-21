c05be857150d11ee63cc338a0b7dc9372b7e9f89
Nikhil Arun Nidhan <nikhilar@maveric-systems.com>
2022-03-21 23:23:44 +0530

    Post Method

--- src/main/java/com/commentservice/Controler/CommentController.java
+++ src/main/java/com/commentservice/Controler/CommentController.java
@@ -1,12 +1,25 @@
 package com.commentservice.Controler;
 
+import com.commentservice.Model.CommentModel;
 import com.commentservice.Service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class CommentController {
 
     @Autowired
     private CommentService commentService;
+
+    @PostMapping("/posts/{postId}/comments")
+    public ResponseEntity<CommentModel> postComment(@Valid @RequestBody CommentModel commentModel, @PathVariable("postId") String postId) {
+        return new ResponseEntity<>(commentService.postComment(commentModel, postId), HttpStatus.ACCEPTED);
+    }
 }
--- src/main/java/com/commentservice/Service/CommentService.java
+++ src/main/java/com/commentservice/Service/CommentService.java
@@ -1,12 +1,22 @@
 package com.commentservice.Service;
 
+import com.commentservice.Model.CommentModel;
 import com.commentservice.Repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class CommentService {
 
     @Autowired
     private CommentRepository commentRepository;
+
+    public CommentModel postComment(CommentModel commentModel, String postId) {
+        commentModel.setPostID(postId);
+        commentModel.setCreatedAt(LocalDateTime.now());
+        commentModel.setUpdatedAt(LocalDateTime.now());
+        return commentRepository.save(commentModel);
+    }
 }
